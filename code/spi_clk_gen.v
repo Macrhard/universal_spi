502121d3156b35df12ba70cddc6071a47a63716f
Macrhard <geetianya@gmail.com>
2019-05-09 02:37:42 -0400

    spi_clk_gen module is ok

--- code/spi_clk_gen.v
+++ code/spi_clk_gen.v
@@ -12,10 +12,7 @@ module spi_clk_gen(
     output      o_neg_edge //pluse marking negative edge of clk_out
 );
 
-parameter Tp = 0;  
-
-reg test_pos_1;
-reg test_pos_3;
+parameter Tp = 0;
 
 reg r_pos_edge_1;   //偶数分频上升沿 clk1上升沿
 reg r_pos_edge_3;   //奇数分频上升沿 clk3上升沿
@@ -33,6 +30,7 @@ reg [`SPI_DIVIDER_LEN-1:0] r_cnt3;
 wire w_clk0;   //system clk 的反时钟
 assign w_clk0 = ~i_clk;
 
+//clk1 2 3是在传输信号启动后产生，传输结束就不再产生时钟 //但cnt 1 2 3 仍要计数
 //clk1 主线
 always @(posedge i_clk or negedge i_rst_n) begin
     if(!i_rst_n) begin
@@ -40,22 +38,33 @@ always @(posedge i_clk or negedge i_rst_n) begin
         r_cnt1 <=  {`SPI_DIVIDER_LEN{1'b0}};
         r_pos_edge_1 <= 1'b0;
         r_neg_edge_1 <= 1'b0;
-        test_pos_1 <= 1'b0;
     end
-    //clk1 2 3是在传输信号启动后产生，传输结束就不再产生时钟 //但cnt 1 2 3 仍要计数
+    else if(i_divider == 'd2) begin
+        if(r_cnt1 >= i_divider-{{`SPI_DIVIDER_LEN-1{1'b0}},1'b1}) begin
+            r_cnt1 <=  {`SPI_DIVIDER_LEN{1'b0}};
+            r_clk1 <=  (i_enable && (!i_last_clk)) ? 1'b1 : 1'b0;  //cnt1 == 1 时clk1上升沿
+            r_pos_edge_1 <= (i_enable && (!i_last_clk)) ? 1'b1 : 1'b0;
+        end
+        else begin
+            r_cnt1 <=  r_cnt1 + {{`SPI_DIVIDER_LEN-1{1'b0}},1'b1};
+            r_clk1 <=  (i_enable && (!i_last_clk)) ? r_clk1 + 1'b1 : 1'b0;
+            r_pos_edge_1 <=  (i_enable && (!i_last_clk)) ? r_pos_edge_1 +1'b1 : 1'b0;
+        end
+    end
     else if(r_cnt1 >= i_divider-{{`SPI_DIVIDER_LEN-1{1'b0}},1'b1}) //cnt 最大到 divider-1
     begin
         r_cnt1 <=  {`SPI_DIVIDER_LEN{1'b0}};
         r_clk1 <=  (i_enable && (!i_last_clk)) ? 1'b0 : 1'b0;  //或上一个w_clk_out的作用是将out_clk多输出一个系统时钟 之后置零
-        r_pos_edge_1 <=  1'b0; //ok
-        r_neg_edge_1 <=  1'b1;
+        r_pos_edge_1 <= 1'b0;
+        if(i_divider[0])  //当分频为奇数是才记录clk1的下降沿
+            r_neg_edge_1 <= (i_enable && (!i_last_clk)) ? 1'b1 : 1'b0;
+        else
+            r_neg_edge_1 <= 1'b0;
     end
-    else if(i_divider[0] == 1'b1) begin //奇数分频
+    else if(i_divider[0]) begin //奇数分频
         if(r_cnt1 == (i_divider-{{`SPI_DIVIDER_LEN-1{1'b0}},1'b1})/2) begin
             r_cnt1 <=  r_cnt1 + {{`SPI_DIVIDER_LEN-1{1'b0}},1'b1};
             r_clk1 <=  (i_enable && (!i_last_clk)) ? 1'b1 : 1'b0;
-            r_pos_edge_1 <= 1'b1;
-            r_neg_edge_1 <= 1'b0;
         end
         else begin
             r_cnt1 <=  r_cnt1 + {{`SPI_DIVIDER_LEN-1{1'b0}},1'b1};
@@ -63,19 +72,18 @@ always @(posedge i_clk or negedge i_rst_n) begin
             r_neg_edge_1 <= 1'b0;
         end  
     end
-    else begin
+    else begin //偶数分频
         if(r_cnt1 == (i_divider/2)) begin
             r_cnt1 <= r_cnt1 + {{`SPI_DIVIDER_LEN-1{1'b0}},1'b1};
             r_clk1 <= (i_enable && (!i_last_clk)) ? 1'b1 : 1'b0;
-            r_pos_edge_1 <= 1'b1;
-            r_neg_edge_1 <= 1'b1;
-            test_pos_1 <= 1'b1; 
+            r_pos_edge_1 <= (i_enable && (!i_last_clk)) ? 1'b1 : 1'b0;
+            r_neg_edge_1 <= 1'b0;
         end
-        else
+        else begin //未达到翻转点继续累加
             r_cnt1 <=  r_cnt1 + {{`SPI_DIVIDER_LEN-1{1'b0}},1'b1};
             r_pos_edge_1 <= 1'b0;
-            r_neg_edge_1 <= 1'b0;
-            test_pos_1 <= 1'b0;
+            r_neg_edge_1 <= 1'b0;  
+        end           
     end
 end
 
@@ -89,13 +97,13 @@ always @(posedge i_clk or negedge i_rst_n) begin
     else if(i_divider == 'd2) begin //添加2分频
         if(r_cnt2 >= i_divider-{{`SPI_DIVIDER_LEN-1{1'b0}},1'b1}) begin
             r_cnt2 <=  {`SPI_DIVIDER_LEN{1'b0}};
-            r_clk2 <=  (i_enable && (!i_last_clk)) ? r_clk2 + 1'b1 : 1'b0;
+            r_clk2 <=  (i_enable && (!i_last_clk)) ? 1'b1 : 1'b0;
             r_neg_edge_2 <=  (i_enable && (!i_last_clk)) ? r_neg_edge_2 +1'b1 : 1'b0;
         end
         else begin
             r_cnt2 <=  r_cnt2 + {{`SPI_DIVIDER_LEN-1{1'b0}},1'b1};
             r_clk2 <=  (i_enable && (!i_last_clk)) ? r_clk2 + 1'b1 : 1'b0;
-            r_neg_edge_2 <=  (i_enable && (!i_last_clk)) ? r_neg_edge_2 +1'b1 : 1'b0;
+            r_neg_edge_2 <=  (i_enable && (!i_last_clk)) ? 1'b1 : 1'b0;
         end
     end
     else if(i_divider == 'd4) begin  //修复4分频bug
@@ -109,14 +117,20 @@ always @(posedge i_clk or negedge i_rst_n) begin
             r_clk2 <=  (i_enable && (!i_last_clk)) ? 1'b1 : 1'b0;
             r_neg_edge_2 <=  1'b0;
         end
-        else 
+        else begin
             r_cnt2 <=  r_cnt2 + {{`SPI_DIVIDER_LEN-1{1'b0}},1'b1};
             r_neg_edge_2 <=  1'b0;
+        end
     end
     else if(r_cnt2 == {`SPI_DIVIDER_LEN{1'b0}}) begin
         r_cnt2 <=  r_cnt2 + {{`SPI_DIVIDER_LEN-1{1'b0}},1'b1};
         r_clk2 <=  (i_enable && (!i_last_clk)) ? 1'b0 : 1'b0;
-        r_neg_edge_2 <=  (i_enable && (!i_last_clk)) ? 1'b1 : 1'b0;
+        if(i_divider[0] == 1'b0) begin
+            r_neg_edge_2 <=  (i_enable && (!i_last_clk)) ? 1'b1 : 1'b0;
+        end
+       else begin
+            r_neg_edge_2 <= 1'b0;
+       end
     end
     else if(r_cnt2 >= (i_divider-{{`SPI_DIVIDER_LEN-1{1'b0}},1'b1})) begin
         r_cnt2 <=  {`SPI_DIVIDER_LEN{1'b0}};
@@ -146,7 +160,6 @@ always @(posedge w_clk0 or negedge i_rst_n) begin
         r_clk3 <=  1'b0;
         r_cnt3 <=  {`SPI_DIVIDER_LEN{1'b0}};
         r_pos_edge_3 <=  1'b0;
-        test_pos_3 <= 1'b0;
     end
     else if(r_cnt3 >= (i_divider-{{`SPI_DIVIDER_LEN-1{1'b0}},1'b1})) begin
         r_cnt3 <=  {`SPI_DIVIDER_LEN{1'b0}};
@@ -157,8 +170,7 @@ always @(posedge w_clk0 or negedge i_rst_n) begin
         if(r_cnt3 == (i_divider-{{`SPI_DIVIDER_LEN-1{1'b0}},1'b1})/2) begin
             r_cnt3 <=  r_cnt3 + {{`SPI_DIVIDER_LEN-1{1'b0}},1'b1};
             r_clk3 <=  (i_enable && (!i_last_clk)) ? 1'b1 : 1'b0;
-            r_pos_edge_3 <=  1'b1;
-            test_pos_3 <= 1'b1;
+            r_pos_edge_3 <= (i_enable && (!i_last_clk)) ? 1'b1 : 1'b0;
         end
         else begin
             r_cnt3 <=  r_cnt3 + {{`SPI_DIVIDER_LEN-1{1'b0}},1'b1};
@@ -169,27 +181,8 @@ always @(posedge w_clk0 or negedge i_rst_n) begin
         r_cnt3 <=  r_cnt3 + {{`SPI_DIVIDER_LEN-1{1'b0}},1'b1};
         r_clk3 <=  1'b0;
         r_pos_edge_3 <=  1'b0;
-        test_pos_3 <= 1'b1;
     end
 end
-
-/*
-wire w_cnt1_zero; //conter is equal to zero
-wire w_cnt2_one; //conter is equal to one
-assign w_cnt1_zero = (r_cnt1 == {{`SPI_DIVIDER_LEN-1{1'b0}},1'b0});
-assign w_cnt2_max = (r_cnt2 == {{`SPI_DIVIDER_LEN-1{1'b0}},1'b0});
-
-always @(posedge i_clk or negedge i_rst_n) begin
-    if(!i_rst_n) begin
-        r_pos_edge <=  1'b0;
-        r_neg_edge_2 <=  1'b0;
-    end
-    else begin
-        r_pos_edge <=  (i_enable && !w_clk_out && w_cnt2_one) || (!(|i_divider) && w_clk_out) || (!(|i_divider) && i_tx_start && !i_enable);
-        r_neg_edge_2 <=  (i_enable && w_clk_out && w_cnt1_zero) || (!(|i_divider) && !w_clk_out && i_enable);
-    end
-end
-*/
 
 assign o_clk_out = i_divider[0] ? (r_clk1 | r_clk3) : (r_clk1 | r_clk2);
 assign o_pos_edge = i_divider[0] ? r_pos_edge_3 : r_pos_edge_1;
--- sim/sim.do
+++ sim/sim.do
@@ -9,24 +9,22 @@ add wave -noupdate /clk_gen_top_tb/spi_clk_gen/o_clk_out
 add wave -noupdate /clk_gen_top_tb/spi_clk_gen/o_neg_edge
 add wave -noupdate /clk_gen_top_tb/spi_clk_gen/i_last_clk
 add wave -noupdate /clk_gen_top_tb/spi_clk_gen/i_enable
-add wave -noupdate -color Gold /clk_gen_top_tb/spi_clk_gen/r_clk1
-add wave -noupdate /clk_gen_top_tb/spi_clk_gen/r_clk2
 add wave -noupdate /clk_gen_top_tb/spi_clk_gen/r_clk3
 add wave -noupdate /clk_gen_top_tb/spi_clk_gen/r_cnt1
 add wave -noupdate /clk_gen_top_tb/spi_clk_gen/r_cnt2
 add wave -noupdate /clk_gen_top_tb/spi_clk_gen/i_divider
-add wave -noupdate -color {Orange Red} /clk_gen_top_tb/spi_clk_gen/o_clk_out
-add wave -noupdate /clk_gen_top_tb/spi_clk_gen/o_pos_edge
 add wave -noupdate -color {Violet Red} /clk_gen_top_tb/spi_clk_gen/o_neg_edge
-add wave -noupdate /clk_gen_top_tb/spi_clk_gen/r_neg_edge_1
-add wave -noupdate -color {Violet Red} -itemcolor {Medium Violet Red} /clk_gen_top_tb/spi_clk_gen/r_pos_edge_1
 add wave -noupdate /clk_gen_top_tb/spi_clk_gen/r_neg_edge_2
 add wave -noupdate -color Goldenrod -itemcolor Goldenrod /clk_gen_top_tb/spi_clk_gen/r_pos_edge_3
 add wave -noupdate /clk_gen_top_tb/spi_clk_gen/r_cnt3
-add wave -noupdate /clk_gen_top_tb/spi_clk_gen/test_pos_1
-add wave -noupdate /clk_gen_top_tb/spi_clk_gen/test_pos_3
+add wave -noupdate -color {Violet Red} -itemcolor {Medium Violet Red} /clk_gen_top_tb/spi_clk_gen/r_pos_edge_1
+add wave -noupdate /clk_gen_top_tb/spi_clk_gen/r_neg_edge_1
+add wave -noupdate -color {Orange Red} /clk_gen_top_tb/spi_clk_gen/o_clk_out
+add wave -noupdate -color Gold /clk_gen_top_tb/spi_clk_gen/r_clk1
+add wave -noupdate /clk_gen_top_tb/spi_clk_gen/r_clk2
+add wave -noupdate /clk_gen_top_tb/spi_clk_gen/o_pos_edge
 TreeUpdate [SetDefaultTree]
-WaveRestoreCursors {{Cursor 1} {2369 ns} 0}
+WaveRestoreCursors {{Cursor 1} {107 ns} 0}
 quietly wave cursor active 1
 configure wave -namecolwidth 467
 configure wave -valuecolwidth 100
@@ -42,7 +40,7 @@ configure wave -griddelta 40
 configure wave -timeline 0
 configure wave -timelineunits ns
 update
-WaveRestoreZoom {2353 ns} {2417 ns}
+WaveRestoreZoom {97 ns} {149 ns}
 
 
 run 5000ns
